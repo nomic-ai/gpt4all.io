fb532e960138aa1828e34383dfcdaa6f4567676e
KeremP <kerpro26@gmail.com>
2023-04-29 16:29:45 -0400

    minor tweaks to small svg animation

--- frontend/gpt4all.io/src/components/connectors/vertical.jsx
+++ frontend/gpt4all.io/src/components/connectors/vertical.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const Vertical = () => {
-    const path = "m197.32283 25.03412l-0.9763794 241.48032";
+    const path = "m197.32283 25.03412l-0 241.48032";
     const width = 317;
     const height = 200;
 
@@ -14,36 +14,32 @@ const Vertical = () => {
             fill="none"
         >
             <path
+                transform="translate(-20,0)"
                 d={path}
                 stroke="black"
                 strokeOpacity="0.2"
             />
             <path
+                transform="translate(-20,0)"
                 d={path}
                 stroke="url(#pulse-1)"
                 strokeLinecap="round"
                 strokeWidth="2"
             />
-            <defs>
-                <motion.linearGradient
+            <motion.path
+                d={"m197.32283 25.03412-0 20"}
+                stroke="gray"
+                // transform="translate(-20,0)"
                 animate={{
-                    x1: [0, width * 2],
-                    x2: [0, width],
-                    y1: [height, height / 2],
-                    y2: [height * 2, height]
+                    x:[-20, -20],
+                    y:[0, height]
                 }}
                 transition={{
                     duration: 2,
                     repeat: Infinity
                 }}
-                id="pulse-1"
-                gradientUnits="userSpaceOnUse"
-                >
-                <stop stopColor="#2EB9DF" stopOpacity="0" />
-                <stop stopColor="#2EB9DF" />
-                <stop offset="1" stopColor="#9E00FF" stopOpacity="0" />
-                </motion.linearGradient>
-            </defs>
+            >
+            </motion.path>
         </svg>
     )
 }

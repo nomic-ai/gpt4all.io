e011b529177b7c83b16ad5cdc04d0c182e0077f9
KeremP <kerpro26@gmail.com>
2023-05-04 13:53:02 -0400

    download buttons are now dropdowns

--- frontend/gpt4all.io/src/components/download_bar.jsx
+++ frontend/gpt4all.io/src/components/download_bar.jsx
@@ -3,38 +3,61 @@ import { Button } from "./ui/button";
 import {ReactComponent as WindowsLogo} from "../icons/windows.svg";
 import {ReactComponent as OSXLogo} from "../icons/apple.svg";
 import {ReactComponent as UbuntuLogo} from "../icons/ubuntu.svg";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "./ui/dropdown";
 
 const DownloadBar = (
     {
         osName,
-        link,
+        linkMain,
+        linkAlt,
     }
 ) => {
     
     return (
-        <a href={link}>
-            <Button className="w-48 text-md font-semibold">
-                {
-                    osName === "Windows" &&
-                    <WindowsLogo
-                        className="w-4 h-4 mr-1"
-                    />
-                }
-                {
-                    osName === "OSX" &&
-                    <OSXLogo
-                        className="w-4 h-4 mr-2"
-                    />
-                }
-                {
-                    osName === "Ubuntu" &&
-                    <UbuntuLogo
-                        className="w-4 h-4 mr-2"
-                    />
-                }
-                {osName} Installer
-            </Button>
-        </a>
+
+        // <a href={link}>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button className="w-48 text-md font-semibold">
+                    {
+                        osName === "Windows" &&
+                        <WindowsLogo
+                            className="w-4 h-4 mr-1"
+                        />
+                    }
+                    {
+                        osName === "OSX" &&
+                        <OSXLogo
+                            className="w-4 h-4 mr-2"
+                        />
+                    }
+                    {
+                        osName === "Ubuntu" &&
+                        <UbuntuLogo
+                            className="w-4 h-4 mr-2"
+                        />
+                    }
+                    {osName} Installer
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-48 border-none bg-[#0f172a]">
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem className="cursor-pointer my-1 hover:bg-slate-800">
+                            <a href={linkMain} className="font-semibold text-white">{osName} Installer</a>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer my-1 hover:bg-slate-800">
+                            <a href={linkAlt} className="font-semibold text-white">{osName} AVX Only</a>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        // </a>
     )
 };
 

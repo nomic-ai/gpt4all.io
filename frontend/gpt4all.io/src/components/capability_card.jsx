9de3f9f79908f9d9da6bdc7514c718deb824ade4
Ben Schmidt <bmschmidt@gmail.com>
2023-05-05 10:00:13 -0400

    fix overlapping images problem

--- frontend/gpt4all.io/src/components/capability_card.jsx
+++ frontend/gpt4all.io/src/components/capability_card.jsx
@@ -5,35 +5,38 @@ const CapabilityCard = (
         header,
         description,
         gif,
-        still
+        still,
+        alt
     }
-) =>
-{
+) => {
     const [active, setActive] = useState(false);
     const [showModal, setShowModal] = useState(false);
 
-    
-
     return (
-        <div onClick={() => setShowModal(!showModal)} onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} className='cursor-pointer w-full sm:w-[350px] xl:w-[450px] h-[350px] xl:h-[380px] text-center border rounded-md flex flex-col gap-4 relative group'>
+        <div onClick={() => setShowModal(!showModal)} onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)}
+            className='cursor-pointer w-full sm:w-[350px] xl:w-[450px] text-center border rounded-md 
+                   flex flex-col gap-4 relative group'>
             <div className="w-full h-full absolute top-0 left-0 conic"></div>
-            <div className='w-full h-[60%] xl:h-2/3'>
+            <div className='w-full h-[60%] relative'>
                 <img
-                    src={active ? gif: still}
+                    alt={alt || ''}
+                    src={active ? gif : still}
+                    className="object-cover w-full h-full"
                 />
             </div>
-            <div className='flex flex-col gap-2 px-6 sm:px-12'>
+            <div className='flex flex-col gap-2 px-6 sm:px-12 my-auto'>
                 <h2 className='text-xl font-semibold'>{header}</h2>
-                <p className='text-sm font-normal leading normal'>
-                {description}
+                <p className='text-sm font-normal leading-normal'>
+                    {description}
                 </p>
             </div>
-            <div className={`${showModal === true ? "block": "hidden"} fixed left-0 top-0 w-screen h-screen bg-slate-900/70 z-40 flex flex-col justify-center items-center`}>
+            <div className={`${showModal === true ? "block" : "hidden"} fixed left-0 top-0 w-screen h-screen bg-slate-900/70 z-40 flex flex-col justify-center items-center`}>
 
-                    <img
-                        src={gif}
-                        className="w-[800px] rounded-md"
-                    />
+                <img
+                    alt={alt || ''}
+                    src={gif}
+                    className="w-[800px] rounded-md"
+                />
 
             </div>
         </div>

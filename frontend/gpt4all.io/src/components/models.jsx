6f04a5bb0fdaf9b404c17f35c2a488893c451e38
Sarvagya <87168264+goodguysarvagya@users.noreply.github.com>
2023-08-08 21:27:07 +0530

    Changed models.json link to original
    The json was not getting fetched at my end due to some CORS issue so i linked it directly now it is reverted to as it was.

--- frontend/gpt4all.io/src/components/models.jsx
+++ frontend/gpt4all.io/src/components/models.jsx
@@ -3,10 +3,10 @@ import * as DOMPurify from "dompurify";
 import { ArrowBigDownDash } from "lucide-react";
 
 const getModels = async () => {
-  //const response = await fetch("https://gpt4all.io/models/models.json");
-  const response = await fetch(
-    "https://raw.githubusercontent.com/nomic-ai/gpt4all/main/gpt4all-chat/metadata/models.json"
-  );
+  const response = await fetch("https://gpt4all.io/models/models.json");
+  //const response = await fetch(
+    //"https://raw.githubusercontent.com/nomic-ai/gpt4all/main/gpt4all-chat/metadata/models.json"
+  //);
   const jsonData = await response.json();
   return jsonData;
 };
